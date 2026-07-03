4792c8990464152c7bb4775cbdf88e48f06b961f
andre <andredeveloper45@gmail.com>
2026-07-03 20:02:02 -0300

    update the project

--- processa.php
+++ processa.php
@@ -11,6 +11,29 @@ $salvar = mysqli_query($conexao,$sql);
 
 mysqli_close($conexao);
 
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Sistema de Cadastro</title>
+        <link rel="stylesheet" href="css/style_sheets.css">
+    </head>
+    <body>
+        <div class="container">
+            <nav>
+                <ul class="menu">
+                    <a href="index.php"><li>Cadastro</li></a>
+                    <a href="consultas.php"><li>Consultas</li></a>
+                </ul>
+            </nav>
+                <section>
+                    <h1 class="title-1">Confirmação de Cadastro</h1>
+                    <hr><br><br>
+                 </section>
+                <footer id="rodape"><h3 id="titleh3">Universidade Leonardo da Vinci - 2026 Todos os Direitos Reservados</h3></footer>
+        </div>
+        
+    </body>
+</html>
